45c7fbc905a14b30848bb21a120d3ddc432ac690
mattwhitfield <whitfield.mj@gmail.com>
2017-09-13 13:22:52 +0100

    EM-217 updated app files to not use depricated react.proptypes

--- app/containers/Bookings/Details/CaseNotes/filterForm.js
+++ app/containers/Bookings/Details/CaseNotes/filterForm.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import PropTypes from 'prop-types';
+import React from 'react';
 import DatePicker from 'containers/FormContainers/datePicker';
 import { connect } from 'react-redux';
 import { reduxForm, Field, formValueSelector } from 'redux-form/immutable';
