31321b79c87999d9f19946f98e1fe6b5f5272f92
mattwhitfield <whitfield.mj@gmail.com>
2017-09-13 17:13:31 +0100

    EM-218 removed unnecessary keys. Removed unused component

--- app/containers/Bookings/Details/header.js
+++ app/containers/Bookings/Details/header.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import BookingsDetailsHeader from 'components/Bookings/Details/header';
 import BookingsDetailsHeaderMobile from 'components/Bookings/Details/headerMobile';
 
 import { selectHeaderDetail } from '../selectors';
@@ -12,38 +11,26 @@ import { openAddCaseNoteModal, showLargePhoto } from '../actions';
 class Header extends PureComponent { // eslint-disable-line react/prefer-stateless-function
 
   render() {
-    const { headerDetails, openAddCaseNote, deviceFormat } = this.props;
-
-    if (deviceFormat === 'desktop') {
-      return (
-        <BookingsDetailsHeader
-          inmateData={headerDetails}
-          openAddCaseNote={openAddCaseNote}
-          setModalOpen={this.props.setModalOpen}
-          setModalData={this.props.setModalData}
-        />);
-    }
+    const { headerDetails, openAddCaseNote, showPhoto } = this.props;
     return (
       <BookingsDetailsHeaderMobile
         inmateData={headerDetails}
         openAddCaseNote={openAddCaseNote}
-        onImageClick={this.props.showLargePhoto}
-      />);
+        onImageClick={showPhoto}
+      />
+    );
   }
 }
 
 Header.propTypes = {
   headerDetails: PropTypes.object.isRequired,
   openAddCaseNote: PropTypes.func.isRequired,
-  setModalOpen: PropTypes.func.isRequired,
-  setModalData: PropTypes.func.isRequired,
 };
 
 export function mapDispatchToProps(dispatch) {
   return {
     openAddCaseNote: () => dispatch(openAddCaseNoteModal()),
-    showLargePhoto: (imageId) => dispatch(showLargePhoto(imageId)),
-
+    showPhoto: (imageId) => dispatch(showLargePhoto(imageId)),
   };
 }
 
