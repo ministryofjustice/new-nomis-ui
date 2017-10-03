86aced5e2f6a92ca86a68ceee500d487f5e6492b
Jamie Draper <draper65@googlemail.com>
2017-10-03 15:00:26 +0100

    EM-130 Key dates     - Various UI tweaks and fixes.

--- app/containers/Bookings/Details/index.js
+++ app/containers/Bookings/Details/index.js
@@ -26,9 +26,9 @@ import {
 const analyticsService = analyticsServiceBuilder();
 
 const tabData = [
-    { tabId: DETAILS_TABS.OFFENDER_DETAILS, title: 'Offender Details', mobileTitle: 'Offender', component: OffenderDetails, componentMobile: OffenderDetailsMobile },
+    { tabId: DETAILS_TABS.OFFENDER_DETAILS, title: 'Offender details', mobileTitle: 'Offender', component: OffenderDetails, componentMobile: OffenderDetailsMobile },
     { tabId: DETAILS_TABS.ALERTS, title: 'Alerts', mobileTitle: 'Alerts', component: Alerts, componentMobile: Alerts },
-    { tabId: DETAILS_TABS.CASE_NOTES, title: 'Case Notes', mobileTitle: 'Case notes', component: CaseNotes, componentMobile: CaseNotes },
+    { tabId: DETAILS_TABS.CASE_NOTES, title: 'Case notes', mobileTitle: 'Case notes', component: CaseNotes, componentMobile: CaseNotes },
     { tabId: DETAILS_TABS.KEY_DATES, title: 'Key dates', mobileTitle: 'Key dates', component: KeyDates, componentMobile: KeyDates },
 ];
 
