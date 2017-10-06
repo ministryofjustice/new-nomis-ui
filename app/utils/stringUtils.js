c51f5695523236d0823984757de31f1e8716ef00
andrewk <andrew.knight@syscon.net>
2017-10-06 11:03:16 +0100

    EM-267: Fix formatting of double-barreled names.

--- app/utils/stringUtils.js
+++ app/utils/stringUtils.js
@@ -3,10 +3,20 @@ function properCase(word) {
   return ((typeof word === 'string') && (word.length >= 1)) ? word[0].toUpperCase() + word.toLowerCase().slice(1) : word;
 }
 
+/**
+ * Converts a name (first name, last name, middle name, etc.) to proper case equivalent, handling double-barreled names
+ * correctly (i.e. each part in a double-barreled is converted to proper case).
+ * @param name name to be converted.
+ * @returns name converted to proper case.
+ */
+function properCaseName(name) {
+  return isBlank(name) ? '' : name.split('-').map(properCase).join('-');
+}
+
 function isBlank(str) {
   return (!str || /^\s*$/.test(str));
 }
 
 export const toFullName = ({ firstName, lastName, name }) =>
-  !isBlank(name) ? name.split(' ').map(properCase).join(', ') :
-    (!isBlank(lastName) ? `${properCase(lastName)}, ` : '') + (!isBlank(firstName) ? properCase(firstName) : '');
+  !isBlank(name) ? name.split(' ').map(properCaseName).join(', ') :
+    (!isBlank(lastName) ? `${properCaseName(lastName)}, ` : '') + (!isBlank(firstName) ? properCaseName(firstName) : '');
