5bfe2702a577f0d09026050233b062533f6fb2fc
Jamie Draper <draper65@googlemail.com>
2017-12-08 13:45:35 +0000

    EM-347 Web session timeout bug   - Fixed a race condition in the SPA when session timeout occurs   - Improved the legibility of test for empty schedule test   - Typo

--- app/containers/Bookings/Details/QuickLook/tests/schedule.test.js
+++ app/containers/Bookings/Details/QuickLook/tests/schedule.test.js
@@ -3,7 +3,7 @@ import { shallow } from 'enzyme';
 import { Activities } from '../index';
 
 describe('Event component', () => {
-  it('should display a message indicating that there are no schedule', () => {
+  it('should display a message indicating no schedule for today', () => {
     const component = shallow(<Activities />);
 
     expect(component.contains(<label>{"Today's"} schedule is empty</label>)).toBe(true);
