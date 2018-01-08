f3a1a89c0a66770accbfb5a874e0b54cb23d5a83
Michael Willis <michael.willis@syscon.net>
2018-01-08 09:25:38 +0000

    update script

--- promote-to-env.sh
+++ promote-to-env.sh
@@ -1,20 +1,8 @@
 #!/usr/bin/env bash
 
-# if less than two arguments supplied, display usage 
-if [  $# -le 1 ]
-then
-  display_usage
-  exit 1
-fi
- 
-# check whether user had supplied -h or --help . If yes display usage 
-if [[ ( $# == "--help") ||  $# == "-h" ]]
-then
-  display_usage
-  exit 0
-fi
-
-promote_to_env $1 $2
+display_usage() {
+  echo -e "\nUsage: $0 [tag] [dev|stage|preprod|prod]\n"
+}
 
 promote_to_env() {
   TAG=$1
@@ -22,16 +10,39 @@ promote_to_env() {
 
   # Grab the tagged build
   git fetch
-  git checkout $TAG
-  git pull --rebase
+  git checkout ${TAG}
 
   # Push the TAG build to ENV
-  git push --force $TAG origin/deploy-to-${ENV}
+  git push --force origin origin/deploy-to-${ENV} HEAD:deploy-to-${ENV}
 
   # Switch back to master branch
   git checkout master
 }
 
-display_usage() { 
-  echo -e "\nUsage: $0 [tag] [env]\n"
-} 
+# if less than two arguments supplied, display usage 
+if [  $# -le 1 ]
+then
+  display_usage
+  exit 1
+fi
+
+# check whether user had supplied -h or --help . If yes display usage
+if [[ ( $# == "--help") ||  $# == "-h" ]]
+then
+  display_usage
+  exit 0
+fi
+
+TAG=$1
+ENV=$2
+
+if [[ "$ENV" =~ ^(dev|stage|preprod|prod)$ ]]; then
+    echo "Deploying to $ENV"
+else
+    echo "$ENV is not a valid environment"
+fi
+
+promote_to_env ${TAG} ${ENV}
+
+
+
