7b7982bf35d7ca847013a98e4f02d802d96dd29a
Peter Bell <peter.bell@digital.justice.gov.uk>
2018-08-24 14:17:51 +0100

    NN-1099 Live / Regression - Key Dates screen no longer displaying ARD (and possibly other) dates. Handle 404 response for getCategoryAssessment.

--- tests/api/eliteApi.test.js
+++ tests/api/eliteApi.test.js
@@ -1,4 +1,4 @@
-const expect = require('chai').expect;
+const { expect, assert } = require('chai');
 const MockAdapter = require('axios-mock-adapter');
 const clientFactory = require('../../server/api/oauthEnabledClient');
 const eliteApiFactory = require('../../server/api/eliteApi').eliteApiFactory;
@@ -44,5 +44,23 @@ describe('eliteApi tests', () => {
   it('Sends post data', async () => {
     axiosMock.onPost('/test', { test: 'test' }).reply(200, {});
     await eliteApi.post({}, '/test', { test: 'test' });
-  })
+  });
+
+  it('Maps a GET assessment/CATEGORY response status of 404 to a null response', async () => {
+    axiosMock.onGet('api/bookings/1234/assessment/CATEGORY').reply(404);
+    const result = await eliteApi.getCategoryAssessment({}, 1234);
+    expect(result).to.be.null;
+  });
+
+  it('throws an exception for a GET assessment/CATEGORY response status of 401 ', async () => {
+    axiosMock.onGet('api/bookings/1234/assessment/CATEGORY').reply(401);
+
+    try {
+      await eliteApi.getCategoryAssessment({}, 1234);
+    } catch (error) {
+      // OK
+      return;
+    }
+    assert.fail(0,1, 'Expected 401 status to result in a throw');
+  });
 });
