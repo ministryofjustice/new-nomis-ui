b9151b62a409ac6175e2c7ac1279e10ea757e909
lbennett-moj <34441186+lbennett-moj@users.noreply.github.com>
2018-09-17 12:40:23 +0100

    NN-1158 added new returnIEP and returnAlert, plus remove whereabouts link if env var not present (#51)

--- server/config.js
+++ server/config.js
@@ -26,10 +26,10 @@ module.exports = {
     keyworker: {
       url: process.env.KEYWORKER_API_URL || 'http://localhost:8081',
       timeoutSeconds: process.env.KEYWORKER_API_TIMEOUT_SECONDS || 10,
-      ui_url: process.env.OMIC_UI_URL || 'http://localhost:3001',
+      ui_url: process.env.OMIC_UI_URL,
     },
     whereabouts: {
-      ui_url: process.env.WHEREABOUTS_UI_URL || 'http://localhost:3001',
+      ui_url: process.env.WHEREABOUTS_UI_URL,
     },
   },
   hmppsCookie: {
