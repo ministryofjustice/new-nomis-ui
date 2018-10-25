183d0d0a9d4239242ce64ce544e8adf7eaf5b888
Peter Phillips <peter@greenthistle.co.uk>
2018-10-25 12:04:24 +0100

    NN-1285: Enforce eslint label has associated control

--- app/components/ValueWithLabel/index.js
+++ app/components/ValueWithLabel/index.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 const ValueWithLabel = ({ label, children, indent }) => (
   <div className="value-with-label row border-bottom-line">
     <div className="col-lg-6 col-xs-6">
-      <label className={`value-with-label__label ${indent && 'shift-right'}`}>{label}</label>
+      <span className={`value-with-label__label ${indent && 'shift-right'}`}>{label}</span>
     </div>
 
     <div className="col-lg-6 col-xs-6">
