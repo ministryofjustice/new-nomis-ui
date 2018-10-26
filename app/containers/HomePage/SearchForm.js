df8af1007095a81a97bb898bdda109cf849873a3
JamieDraperUK <draper65@googlemail.com>
2018-10-26 14:27:58 +0100

    NN-1243 Fix global search parameter bug

--- app/containers/HomePage/SearchForm.js
+++ app/containers/HomePage/SearchForm.js
@@ -133,7 +133,7 @@ function mapDispatchToProps(dispatch) {
       if (globalSearchUrl)
         window.location.assign(
           `${globalSearchUrl}?${buildQueryString({
-            keywords: formData.keywords,
+            searchText: formData.searchText,
           })}`
         )
       else return dispatch(push(`/results?${buildSearchQueryString(formData)}`))
