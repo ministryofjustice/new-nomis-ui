f01fc13a58086c8cddf8b9ac112a3219f34451e7
JamieDraperUK <draper65@googlemail.com>
2018-12-03 12:34:34 +0000

    NN-1419 Create role based (add key worker session button)  - Fix integration tests

--- notm-specs/src/test/groovy/pages/OffenderDetailsPage.groovy
+++ notm-specs/src/test/groovy/pages/OffenderDetailsPage.groovy
@@ -16,9 +16,9 @@ class OffenderDetailsPage extends Page {
   static content = {
     errors { module(ErrorsModule) }
     offenderNameHeading { $('h1.heading-medium').text() }
-    addCaseNoteLink { $('a.button-link[name="add-case-note-link"]',0) }
-    addKeyworkerSessionLink(required: false) { $('a.button-link[name="add-kw-session-link"]',0) }
-    addAppointmentLink { $('a.button-link[name="add-appointment-link"]',0) }
+    addCaseNoteLink { $('a.button-link[name="add-case-note-link"]').find{element -> element.displayed }}
+    addKeyworkerSessionLink(required: false) { $('a.button-link[name="add-kw-session-link"]').find{element -> element.displayed }}
+    addAppointmentLink { $('a.button-link[name="add-appointment-link"]').find{element -> element.displayed }}
     spinner(required: false) { $('div.spinner-component')}
   }
 }
