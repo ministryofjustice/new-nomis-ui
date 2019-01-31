9441ad5b5f6c1109f8ffd1edf99c49e7ec0f33ff
John Robertson <jsrobertson@users.noreply.github.com>
2019-01-31 17:43:25 +0000

    NN-1624 Fixed case note breaking all words (#174)

--- app/components/Bookings/Details/CaseNotes/CaseNoteListItem.styles.js
+++ app/components/Bookings/Details/CaseNotes/CaseNoteListItem.styles.js
@@ -52,7 +52,6 @@ export const CaseNoteText = styled('pre')`
   margin-bottom: ${SPACING.SCALE_3};
   font-family: ${NTA_LIGHT};
   white-space: pre-wrap;
-  word-break: break-all;
 `
 
 export const CaseNoteAmendmentButton = styled(Link)`
