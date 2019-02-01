3c192680372022e794d1786a56cfeba1d38d9d5a
John Robertson <jsrobertson@gmail.com>
2019-02-01 13:55:14 +0000

    Made footer accept navigation and meta props

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -81,7 +81,138 @@ export class App extends Component {
         </main>
         {/* eslint-disable-next-line */}
         <div onClick={() => this.onBackgroundClick()}>
-          <Footer />
+          <Footer
+            meta={{
+              items: [
+                {
+                  href: '#',
+                  text: 'Help',
+                },
+                {
+                  href: '#',
+                  text: 'Cookies',
+                },
+                {
+                  href: '#',
+                  text: 'Contact',
+                },
+                {
+                  href: '#',
+                  text: 'Terms and conditions',
+                },
+                {
+                  href: '#',
+                  text: 'Rhestr o Wasanaethau Cymraeg',
+                },
+              ],
+              html: (
+                <div>
+                  Built by the <a href="#">Government Digital Service</a>
+                </div>
+              ),
+            }}
+            navigation={[
+              {
+                title: 'Services and information',
+                columns: 2,
+                items: [
+                  {
+                    href: '#',
+                    text: 'Benefits',
+                  },
+                  {
+                    href: '#',
+                    text: 'Births, deaths, marriages and care',
+                  },
+                  {
+                    href: '#',
+                    text: 'Business and self-employed',
+                  },
+                  {
+                    href: '#',
+                    text: 'Childcare and parenting',
+                  },
+                  {
+                    href: '#',
+                    text: 'Citizenship and living in the UK',
+                  },
+                  {
+                    href: '#',
+                    text: 'Crime, justice and the law',
+                  },
+                  {
+                    href: '#',
+                    text: 'Disabled people',
+                  },
+                  {
+                    href: '#',
+                    text: 'Driving and transport',
+                  },
+                  {
+                    href: '#',
+                    text: 'Education and learning',
+                  },
+                  {
+                    href: '#',
+                    text: 'Employing people',
+                  },
+                  {
+                    href: '#',
+                    text: 'Environment and countryside',
+                  },
+                  {
+                    href: '#',
+                    text: 'Housing and local services',
+                  },
+                  {
+                    href: '#',
+                    text: 'Money and tax',
+                  },
+                  {
+                    href: '#',
+                    text: 'Passports, travel and living abroad',
+                  },
+                  {
+                    href: '#',
+                    text: 'Visas and immigration',
+                  },
+                  {
+                    href: '#',
+                    text: 'Working, jobs and pensions',
+                  },
+                ],
+              },
+              {
+                title: 'Departments and policy',
+                items: [
+                  {
+                    href: '#',
+                    text: 'How government works',
+                  },
+                  {
+                    href: '#',
+                    text: 'Departments',
+                  },
+                  {
+                    href: '#',
+                    text: 'Worldwide',
+                  },
+                  {
+                    href: '#',
+                    text: 'Policies',
+                  },
+                  {
+                    href: '#',
+                    text: 'Publications',
+                  },
+                  {
+                    href: '#',
+                    text: 'Announcements',
+                  },
+                ],
+              },
+            ]}
+          />
         </div>
       </div>
     )
