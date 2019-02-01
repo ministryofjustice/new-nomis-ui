a0e17067229dd503e55141a74d00462bea021a86
bell-pa <bell.pa@gmail.com>
2019-02-01 21:05:13 +0000

    Fix some brittle integration tests (#177)
    * NN Fix brittle content selector in AddCaseNotePage
    
    * NN Fix brittle tests
    
    * NN Fix more brittle tests

--- notm-specs/src/test/groovy/pages/AddCaseNotePage.groovy
+++ notm-specs/src/test/groovy/pages/AddCaseNotePage.groovy
@@ -14,11 +14,11 @@ class AddCaseNotePage extends Page {
   static content = {
     errors { module(ErrorsModule) }
     headingText { $("[data-qa=\'page-heading-text\']").text() }
-    form { $('form')}
+    form { $('form') }
     type { $('select', name: 'typeValue') }
     subType { $('select', name: 'subtypeValue') }
     textareaElement { $('textarea') }
-    datePicker { $('div.date-picker-component') } // click this to get picker
+    datePicker(required: false) { $('div.date-picker-component') } // click this to get picker
     days { $('td.rdtDay') } // days on picker, click to set date
     hours { $('select', name: 'hours') } // options 00, 01, 02 etc
     minutes { $('select', name: 'minutes') } // options 00, 05, 10 etc
@@ -53,6 +53,7 @@ class AddCaseNotePage extends Page {
     textarea.text = text
 
     changeDateTime.click()
+    waitFor { datePicker.present }
     datePicker.click()
     days[0].click() // select 1st of this month for now
     form.hours = "07"
