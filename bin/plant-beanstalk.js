3503e3d2d8b1265e9834d5dfdf98a6c479bcd469
Michael Willis <michael.willis@digital.justice.gov.uk>
2019-02-25 12:07:28 +0000

    Fix Linting

--- bin/plant-beanstalk.js
+++ bin/plant-beanstalk.js
@@ -10,14 +10,12 @@ if (!version) {
 const dockerrun = {
   AWSEBDockerrunVersion: '1',
   Image: {
-    Name: 'mojdigitalstudio/new-nomis-ui:' + version,
+    Name: `mojdigitalstudio/new-nomis-ui:${version}`,
     Update: 'true',
   },
   Ports: [{ ContainerPort: '3000' }],
 }
 
 const output = JSON.stringify(dockerrun, null, 2)
-
-console.log(output)
 
 fs.writeFileSync(path.resolve(__dirname, '../Dockerrun.aws.json'), output)
