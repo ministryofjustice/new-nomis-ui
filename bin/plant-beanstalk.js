524041d35f671e79db1fa0630132eced7637c9a2
Michael Willis <michael.willis@digital.justice.gov.uk>
2019-02-26 16:28:03 +0000

    Notm to aws (#194)
    * Circle CI stage build on AWS using EB
    
    * Fix Linting
    
    * Fix config for ELB
    
    * Fix config for ELB deploy to dev
    
    * Readyness for AWS switch

--- bin/plant-beanstalk.js
+++ bin/plant-beanstalk.js
@@ -10,15 +10,12 @@ if (!version) {
 const dockerrun = {
   AWSEBDockerrunVersion: '1',
   Image: {
-    Name: `sysconjusticesystems/elite2-web:${version}`,
+    Name: `mojdigitalstudio/new-nomis-ui:${version}`,
     Update: 'true',
-  },
-  Authentication: {
-    Bucket: 'docker.private.repos.auth',
-    Key: 'sysconjusticesysytems/.dockercfg',
   },
   Ports: [{ ContainerPort: '3000' }],
 }
 
 const output = JSON.stringify(dockerrun, null, 2)
+
 fs.writeFileSync(path.resolve(__dirname, '../Dockerrun.aws.json'), output)
