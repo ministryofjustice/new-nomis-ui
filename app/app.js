47bf601fe857e86e357eaadbdf4b5438ee5fb6b5
John Robertson <jsrobertson@gmail.com>
2019-03-07 15:50:19 +0000

    NN-1635 Upgrade to babel 7

--- app/app.js
+++ app/app.js
@@ -11,9 +11,6 @@ import '!file-loader?name=[name].[ext]!./manifest.json'
 import 'file-loader?name=[name].[ext]!./.htaccess'
 /* eslint-enable */
 
-// Needed for redux-saga es6 generator support
-import 'babel-polyfill'
-
 // Import all the third party stuff
 import React from 'react'
 import ReactDOM from 'react-dom'
--- internals/testing/test-bundler.js
+++ internals/testing/test-bundler.js
@@ -1,6 +1,3 @@
-// needed for regenerator-runtime
-// (ES7 generator support is required by redux-saga)
-import 'babel-polyfill'
 import { configure } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 
