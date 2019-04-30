43bb94375f4ea8b3eb94fd92d99e29f938ff5068
John Robertson <jsrobertson@gmail.com>
2019-04-30 11:21:19 +0100

    Changed how bundle stats are run to be inline with other apps

--- internals/webpack/webpack.prod.babel.js
+++ internals/webpack/webpack.prod.babel.js
@@ -1,7 +1,35 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+// eslint-disable-next-line import/no-extraneous-dependencies
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
+
+const isAnalyse = process.env.BUNDLE_ANALYSE !== undefined
+
+const plugins = [
+  new webpack.optimize.LimitChunkCountPlugin({
+    maxChunks: 1,
+  }),
+  // Minify and optimize the index.html
+  new HtmlWebpackPlugin({
+    template: 'app/index.html',
+    minify: {
+      removeComments: true,
+      collapseWhitespace: true,
+      removeRedundantAttributes: true,
+      useShortDoctype: true,
+      removeEmptyAttributes: true,
+      removeStyleLinkTypeAttributes: true,
+      keepClosingSlash: true,
+      minifyJS: true,
+      minifyCSS: true,
+      minifyURLs: true,
+    },
+    inject: true,
+  }),
+]
+
+if (isAnalyse) plugins.push(new BundleAnalyzerPlugin())
 
 module.exports = require('./webpack.base.babel')({
   entry: [path.join(process.cwd(), 'app/app.js')],
@@ -10,32 +38,7 @@ module.exports = require('./webpack.base.babel')({
     filename: 'client.js',
   },
 
-  plugins: [
-    new webpack.optimize.LimitChunkCountPlugin({
-      maxChunks: 1,
-    }),
-    // Minify and optimize the index.html
-    new HtmlWebpackPlugin({
-      template: 'app/index.html',
-      minify: {
-        removeComments: true,
-        collapseWhitespace: true,
-        removeRedundantAttributes: true,
-        useShortDoctype: true,
-        removeEmptyAttributes: true,
-        removeStyleLinkTypeAttributes: true,
-        keepClosingSlash: true,
-        minifyJS: true,
-        minifyCSS: true,
-        minifyURLs: true,
-      },
-      inject: true,
-    }),
-    new BundleAnalyzerPlugin({
-      generateStatsFile: true,
-      analyzerMode: 'disabled',
-    }),
-  ],
+  plugins,
 
   performance: {
     assetFilter: assetFilename => !/(\.map$)|(^(main\.|favicon\.))/.test(assetFilename),
