e92a7c331af3e1371f74f4164ffc31b18a93f50c
Peter Phillips <peter@greenthistle.co.uk>
2019-10-02 12:26:15 +0100

    DT-253: Add ping endpoint and test

---
+++ notm-specs/src/test/groovy/specs/PingSpecification.groovy
@@ -0,0 +1,26 @@
+package specs
+
+import groovyx.net.http.HttpBuilder
+import spock.lang.Specification
+
+import static groovyx.net.http.HttpBuilder.configure
+
+class PingSpecification extends Specification {
+
+    HttpBuilder http
+
+    def setup() {
+        http = configure {
+            request.uri = 'http://localhost:3006/ping'
+        }
+    }
+
+    def "Ping page returns pong"() {
+        given:
+
+        when:
+        def response = this.http.get()
+        then:
+        new String(response) == 'pong'
+    }
+}
