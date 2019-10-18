c559542531796eb58fd13e0da852fa5c37d9334b
JamieDraperUK <draper65@googlemail.com>
2019-10-18 16:43:30 +0100

    NN-2203 fix logout issue

--- server/sessionManagementRoutes.js
+++ server/sessionManagementRoutes.js
@@ -26,9 +26,9 @@ const configureRoutes = ({ app, tokenRefresher, mailTo }) => {
   }
 
   const logout = (req, res) => {
-    req.logout()
-    req.session = null
-    res.redirect(authLogoutUrl)
+    req.session.destroy(() => {
+      res.redirect(authLogoutUrl)
+    })
   }
 
   /**
