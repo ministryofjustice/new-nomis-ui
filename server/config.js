44572046c8468f7dcfec3d88b69320a8b5a30cf8
Nevelina Aleksandrova <nevelina.aleksandrova@digital.justice.gov.uk>
2019-11-20 12:12:54 +0000

    NN-2277: Integrate with allocation management API, add to healthcheck

--- server/config.js
+++ server/config.js
@@ -40,6 +40,10 @@ const apis = {
     url: process.env.API_ENDPOINT_URL || 'http://localhost:8080/',
     timeoutSeconds: process.env.API_ENDPOINT_TIMEOUT_SECONDS || 30,
   },
+  allocationManager: {
+    url: process.env.ALLOCATION_MANAGER_ENDPOINT_URL,
+    timeoutSeconds: process.env.API_ENDPOINT_TIMEOUT_SECONDS || 30,
+  },
   keyworker: {
     url: process.env.KEYWORKER_API_URL || 'http://localhost:8081/',
     timeoutSeconds: process.env.KEYWORKER_API_TIMEOUT_SECONDS || 30,
