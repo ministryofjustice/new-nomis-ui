0e07adb79fe569e5c459bd2037e03f21711afd56
John Robertson <jsrobertson@gmail.com>
2020-01-08 11:27:58 +0000

    NN-2361 Change Incentives to Incentive

--- app/containers/Bookings/Details/QuickLook/index.js
+++ app/containers/Bookings/Details/QuickLook/index.js
@@ -185,7 +185,7 @@ export const NegativeAndPositiveCaseNoteCount = ({ negativeCaseNotes, positiveCa
 
     <div className="row border-bottom-line">
       <div className="col-lg-6 col-xs-6">
-        <span>Incentives Encouragement</span>
+        <span>Incentive Encouragement</span>
       </div>
 
       <div className="col-lg-6 col-xs-6">
