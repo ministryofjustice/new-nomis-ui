c6c733c5a4f66875ea07be3c24df29cd39080d68
Nevelina Aleksandrova <nevelina.aleksandrova@digital.justice.gov.uk>
2020-07-03 09:45:32 +0100

    NN-2756: Fix page render failure when coming from non-react page (#487)

--- app/containers/Bookings/Details/CaseNotes/AmendCaseNote/AmendCaseNote.js
+++ app/containers/Bookings/Details/CaseNotes/AmendCaseNote/AmendCaseNote.js
@@ -2,8 +2,6 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { reduxForm, Field } from 'redux-form/immutable'
 import { connect } from 'react-redux'
-import { Link } from 'react-router-dom'
-import ReactRouterPropTypes from 'react-router-prop-types'
 import { createFormAction } from 'redux-form-saga'
 import { Map } from 'immutable'
 import GridRow from '@govuk-react/grid-row'
@@ -12,12 +10,12 @@ import Button from '@govuk-react/button'
 
 import SessionHeartbeatHandler from '../../../../../utils/sessionHeartbeatHandler'
 import { TextArea, ButtonCancel } from '../../../../../components/FormComponents'
-import { AMEND_CASENOTE } from '../../../constants'
-import { extendActiveSession } from '../../../actions'
+import { DETAILS_TABS, AMEND_CASENOTE } from '../../../constants'
+import { viewDetails, extendActiveSession } from '../../../actions'
 import Page from '../../../../../components/Page'
 import { ButtonContainer } from './AmendCaseNote.styles'
 
-const AmendCaseNote = ({ handleSubmit, error, submitting, extendSession, location }) => {
+const AmendCaseNote = ({ handleSubmit, error, submitting, extendSession, goBackToBookingDetails }) => {
   if (error) {
     window.scrollTo(0, 0)
   }
@@ -47,7 +45,12 @@ const AmendCaseNote = ({ handleSubmit, error, submitting, extendSession, locatio
                 <Button type="submit" disabled={error || submitting}>
                   Save amendment
                 </Button>
-                <ButtonCancel as={Link} to={location.state.from}>
+                <ButtonCancel
+                  onClick={e => {
+                    e.preventDefault()
+                    goBackToBookingDetails()
+                  }}
+                >
                   Cancel
                 </ButtonCancel>
               </ButtonContainer>
@@ -63,7 +66,6 @@ AmendCaseNote.propTypes = {
   handleSubmit: PropTypes.func.isRequired,
   error: PropTypes.string,
   submitting: PropTypes.bool.isRequired,
-  location: ReactRouterPropTypes.location.isRequired,
 
   // mapDispatchToProps
   extendSession: PropTypes.func.isRequired,
@@ -73,21 +75,29 @@ AmendCaseNote.defaultProps = {
   error: '',
 }
 
-const mapDispatchToProps = (dispatch, props) => ({
-  extendSession: () => dispatch(extendActiveSession()),
-  onSubmit: createFormAction(
-    formData => ({
-      type: AMEND_CASENOTE.BASE,
-      payload: {
-        ...formData.toJS(),
-        offenderNo: props.match.params.offenderNo,
-        caseNoteId: props.match.params.caseNoteId,
-        caseNoteListReferrer: props.location.state.from,
-      },
-    }),
-    [AMEND_CASENOTE.SUCCESS, AMEND_CASENOTE.ERROR]
-  ),
-})
+const mapDispatchToProps = (dispatch, props) => {
+  const {
+    match: {
+      params: { offenderNo },
+    },
+  } = props
+  return {
+    extendSession: () => dispatch(extendActiveSession()),
+    onSubmit: createFormAction(
+      formData => ({
+        type: AMEND_CASENOTE.BASE,
+        payload: {
+          ...formData.toJS(),
+          offenderNo: props.match.params.offenderNo,
+          caseNoteId: props.match.params.caseNoteId,
+          caseNoteListReferrer: props.location.state.from,
+        },
+      }),
+      [AMEND_CASENOTE.SUCCESS, AMEND_CASENOTE.ERROR]
+    ),
+    goBackToBookingDetails: () => dispatch(viewDetails(offenderNo, DETAILS_TABS.CASE_NOTES)),
+  }
+}
 
 const mapStateToProps = (state, props) => ({
   offenderNo: props.match.params.offenderNo,
