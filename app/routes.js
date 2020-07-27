6be2d195f79afc815b09da92ce152e220654b8b3
Nevelina Aleksandrova <nevelina.aleksandrova@digital.justice.gov.uk>
2020-07-27 16:36:23 +0100

    NN-2708: Put redirects in place for offender schedule and add a case note (#505)

--- app/routes.js
+++ app/routes.js
@@ -1,14 +1,8 @@
 import { connect } from 'react-redux'
 
 import HomePage from './containers/HomePage'
-import AddCaseNote from './containers/Bookings/Details/AddCaseNote'
-import ResultsContainer from './containers/Bookings/Results'
-import Details from './containers/Bookings/Details'
-import ScheduledEvents from './containers/Bookings/Details/ScheduledEvents'
 import AmendCaseNote from './containers/Bookings/Details/CaseNotes/AmendCaseNote'
 import NotFoundPage from './containers/NotFoundPage'
-
-import { selectPrisonStaffHubUrl } from './selectors/app'
 
 export default [
   {
@@ -18,26 +12,9 @@ export default [
     component: HomePage,
   },
   {
-    exact: true,
-    path: '/offenders/:offenderNo/add-case-note',
-    name: 'addCaseNote',
-    component: AddCaseNote,
-  },
-  {
-    exact: true,
-    path: '/offenders/:offenderNo/schedule',
-    name: 'schedule',
-    component: ScheduledEvents,
-  },
-  {
     path: '/offenders/:offenderNo/case-notes/:caseNoteId/amend-case-note',
     name: 'amendCaseNote',
     component: AmendCaseNote,
-  },
-  {
-    path: '/results',
-    name: 'searchResults',
-    component: ResultsContainer,
   },
   {
     path: '/offenders/:offenderNo',
