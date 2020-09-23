3cc45925c9d1aa376302a85781fe6b9249a0e367
John Robertson <jsrobertson@gmail.com>
2020-09-23 09:10:09 +0100

    NN-2857 Fix linting issues

--- app/components/ActionLinks/index.js
+++ app/components/ActionLinks/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Link } from 'react-router-dom'
 import './index.scss'
 
 export const ActionLink = ({ url, testId, image, children, ...rest }) => (
